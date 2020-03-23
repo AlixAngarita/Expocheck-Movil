9fa7be93d6069d364272875203ad9001b703f2ad
Deiver <deiver.guerra.2017@upb.edu.co>
2020-03-23 01:49:50 -0500

    Interfaz presentacion implementada

--- src/components/Presentacion/presentacion.component.js
+++ src/components/Presentacion/presentacion.component.js
@@ -1,108 +1,244 @@
 import React from "react";
-import ActionButton from 'react-native-action-button';
-import { withNavigation } from 'react-navigation';
+import { Dimensions } from "react-native";
+import { withNavigation } from "react-navigation";
+import { Avatar, Card, Badge, Divider  } from "react-native-elements";
+import FlipCard from 'react-native-flip-card'
 import {
   View,
   StyleSheet,
   FlatList,
   Text,
   ActivityIndicator,
-  Alert
-  
+  Alert,
+  ScrollView,
 } from "react-native";
-import { Rating,Icon} from "react-native-elements";
-import moment from 'moment';
-
+import { Rating, Icon , AirbnbRating} from "react-native-elements";
+import moment from "moment";
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  titleContainer: {
-    flex:1,
+  loading: {
+    flex: 1,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    backgroundColor: "white",
-    elevation: 5,
-    margin:20
-  },
-
-  commentsContainer: {
-    flex: 1
-  },
-  comment:{
-    color:'grey'
-  },
-  comentStyle:{
-    marginHorizontal:20,
-    marginVertical:5, 
-    borderRadius:20,
-    padding:10,
-    backgroundColor:'#f1f2f6'
-  },
-  loading:{
-    flex:1,
-    flexDirection:'column', 
-    alignItems:'center', 
-    justifyContent:'center'
-}
+    justifyContent: "center"
+  }
 });
-
+import Calificar from '../btnCalifcar/btnCalificar.component'
 
 const keyExtractor = (item, index) => index.toString();
 
 const renderItem = ({ item }) => (
-    
-      <View style={styles.comentStyle}>
-        <Text style={styles.comment}>
-          {item}
-        </Text>
-      </View>
+  <View style={styles.comentStyle}>
+    <Text style={styles.comment}>{item.contenido}</Text>
+  </View>
 );
 
-
- 
-
-
+const width = Dimensions.get("window").width;
 const Presentacion = props => {
- 
   return (
     <View style={styles.container}>
-    
-      
-      {props.presentacion != '' && !props.loading ?
-        <React.Fragment>
-            <View style={styles.titleContainer}>
-                <Text style={{fontSize:20, textAlign:'center',marginBottom:20}}>{props.presentacion.titulo}</Text>
-                <Rating
-                      imageSize={25}
-                      type="custom"
-                      readonly
-                      fractions={1}
-                      startingValue={props.presentacion.calificacion.length > 0 ? (props.presentacion.calificacion.reduce((a, b) => a + b, 0))/props.presentacion.calificacion.length:0}
-                    />
-                  <Text style={{fontSize:10, color:'grey'}}>{'('+props.presentacion.calificacion.length+' votaciones )'}</Text>
+      {props.presentacion != "" && !props.loading && (
+        <ScrollView showsVerticalScrollIndicator={false}>
+           <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              alignContent: "center"
+            }}
+          >
+            <Card
+              style={{
+                marginHorizontal:20,
+                marginTop: 10
+              }}
+            >
+              <Text style={{  fontSize: 25 }}>
+                {props.presentacion.titulo}
+              </Text>
+              <Text style={{ color: "grey", fontSize: 20 }}>Integrantes</Text>
+              <View style={{ alignItems: "center" }}>
+                {props.presentacion.integrantes.map((integrante, i) => (
+                  <View
+                   key={i.toString()}
+                    style={{
+                      marginTop:5,
+                      width:'100%',
+                      borderRadius: 20,
+                      padding: 5,
+                      flexWrap: "nowrap",
+                      borderColor:'#DCDDE1',
+                      borderStyle:'solid',
+                      borderWidth:1
+                    }}
+                  >
+                    <View style={{ flexDirection: "row", padding:2 }}>
+                      <View style={{ marginHorizontal: 10, justifyContent: "center"}}>
+                        <View>
+                          <Avatar
+                            size="small"
+                            rounded
+                            title={(i + 1).toString()}
+                            activeOpacity={0.7}
+                            containerStyle={{
+                              borderColor: "#95A5A6",
+                              borderStyle: "dashed",
+                              borderWidth: 1
+                            }}
+                          />
+                        </View>
+                      </View>
+                      <View style={{ justifyContent: "center" }}>
+                        <Text style={{ color: "#95A5A6", fontSize: 18 }}>
+                          {integrante.nombre}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+                ))}
+              </View>
+            
+             <Calificar/>
+            </Card>
+
+            <View style={{marginTop:30}}>
+              <View style={{flexDirection:'row'}}>
+                <View style={{justifyContent:'center'}}>
+                  <Icon
+                  name='check'
+                  type='font-awesome'
+                  color='#fbc531' />
+                </View>
+                <Text style={{color:'grey', textAlign:'center', fontSize:20,marginBottom:10,
+                  marginLeft:10}}>Calificaciones del auditorio</Text>
+              </View>
+              <Divider style={{marginBottom:5}}/>
             </View>
 
-            <View style={styles.commentsContainer}>
-              {props.presentacion.comentarios.length > 0 ?
-                <FlatList
-                keyExtractor={keyExtractor}
-                data={props.presentacion.comentarios}
-                renderItem={renderItem}
-              /> :
-              <View style={{ display:'flex',flex:1, flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-                <Text style={{color:'grey'}}>Sin comentarios.</Text>
-              </View>
-              }
-                <ActionButton
+            <View style={{ flexDirection: "row", padding:2,
+            width:'90%',
+            marginTop:10,
+            borderRadius:20,
+            backgroundColor:'#48DBFB'}}>
+                <View style={{ marginHorizontal: 10, justifyContent: "center"}}>
+                  <View>
+                    <Icon
+                    raised
+                    name='arrow-up'
+                    type='font-awesome'
+                    color='#44BD32'
+                    onPress={() => console.log('hello')} />
+                  </View>
+                </View>
+                <View style={{ justifyContent: "center" }}>
+                  <Text style={{ color: "white", fontSize: 18 }}>
+                    Por encima de la media
+                  </Text>
+                </View>
+             </View>
+
+            {props.evaluacionPublica ? (
+              <ScrollView  showsVerticalScrollIndicator={false}>
+                {props.evaluaciones.map((evaluacion, i) => (
+                   <FlipCard
+                      style={{marginBottom:10}}
+                      key={(i).toString()}>
+                      {/* Face Side */}
+                      <View  style={{ padding:2,
+                      width:width-30,
+                      marginTop:10,
+                      borderRadius:20,
+                      borderColor:'#95A5A6',
+                      borderStyle:'solid',
+                      borderWidth:1 }}>
+                            <Text style={{textAlign:'center', fontSize:20}}>{evaluacion.metrica} </Text>
+                            <View  style={{ alignItems:'center'}}>
+                              <Rating
+                                  startingValue={evaluacion.calificaciones.length > 0 ? (evaluacion.calificaciones.reduce((a, b) => a+b,0))/evaluacion.calificaciones.length:0}
+                                  imageSize={30}
+                                  type="custom"
+                                  readonly
+                                  fractions={0.5}
+                                                />
+                            </View>
+                    </View>
+                      {/* Back Side */}
+                      <View  style={{ padding:2,
+                      width:width-30,
+                      marginTop:10,
+                      borderRadius:20,
+                      borderColor:'#FFC312',
+                      borderStyle:'solid',
+                      borderWidth:1 }}>
+                            <View  style={{ alignItems:'center'}}>
+                              <AirbnbRating
+                                  count={5}
+                                  reviews={['Mala','Regular','Aceptable','Buena','Excelente']}
+                                  imageSize={30}
+                                  type="custom"
+                                  fractions={0.5}
+                                                />
+                            </View>
+                    </View>
+                           
+                  </FlipCard>
+                 
+                ))}
+              </ScrollView>
+            ):(
+              <ScrollView showsVerticalScrollIndicator={false}>
+                {props.evaluaciones.map((evaluacion, i) => (
+                  <FlipCard
+                    style={{marginBottom:10}}
+                    key={(i+3).toString()}>
+                      <View style={{ flexDirection: "row", padding:2,
+                      width:width-30,
+                      marginTop:10,
+                      borderRadius:20,
+                      backgroundColor:'#FFC312' }}>
+                          <View style={{ marginHorizontal: 10, justifyContent: "center"}}>
+                            <View>
+                              <Icon
+                              raised
+                              name='arrow-up'
+                              type='font-awesome'
+                              color='#44BD32'
+                              onPress={() => console.log('hello')} />
+                            </View>
+                          </View>
+                          <View style={{ justifyContent: "center" }}>
+                            <Text style={{ color: "white", fontSize: 18 }}>
+                              {evaluacion.metrica}
+                            </Text>
+                          </View>
+                      </View>
+
+                      {/* Back Side */}
+                      <View  style={{ padding:2,
+                      width:width-30,
+                      marginTop:10,
+                      borderRadius:20,
+                      borderColor:'#FFC312',
+                      borderStyle:'solid',
+                      borderWidth:1 }}>
+                            <View  style={{ alignItems:'center'}}>
+                              <AirbnbRating
+                                  count={5}
+                                  reviews={['Mala','Regular','Aceptable','Buena','Excelente']}
+                                  imageSize={30}
+                                  type="custom"
+                                  fractions={0.5}
+                                                />
+                            </View>
+                    </View>
+                  </FlipCard>
+                ))}
+              </ScrollView>
+            )}
+          </View>
+
+          {/* <ActionButton
                   offsetY={5}
                   offsetX={20}
                   renderIcon = {(active) => <Icon name="done" color="white"/>}
@@ -113,23 +249,38 @@ const Presentacion = props => {
                       Alert.alert('Fuera de fecha', 'Solo puede ver la presentación.')
                     } 
                   }
-                />
-            </View>
-        </React.Fragment>:null
-      }
+                /> 
+                <Rating
+                      imageSize={25}
+                      type="custom"
+                      readonly
+                      fractions={1}
+                      startingValue={props.presentacion.calificacion.length > 0 ? (props.presentacion.calificacion.reduce((a, b) => a + b, 0))/props.presentacion.calificacion.length:0}
+                    />*/}
+        </ScrollView>
+      )}
 
-      {!props.loading && props.presentacion == '' ?
-        <View style={{ display:'flex',flex:1, flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-          <Text style={{color:'grey', fontSize:17}}>No  hay una presentación agendada.</Text>
-        </View>:null
-      } 
+      {!props.loading && props.presentacion == "" ? (
+        <View
+          style={{
+            display: "flex",
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Text style={{ color: "grey", fontSize: 17 }}>
+            No hay una presentación agendada.
+          </Text>
+        </View>
+      ) : null}
 
-      {props.loading && props.presentacion == '' && 
+      {props.loading && props.presentacion == "" && (
         <View style={styles.loading}>
           <ActivityIndicator size="large" color="#0000ff" />
         </View>
-      }
-
+      )}
     </View>
   );
 };
--- src/redux/reducers/loading.reducers.js
+++ src/redux/reducers/loading.reducers.js
@@ -3,7 +3,6 @@ import {loadingJornadas} from '../actions/loading.actions'
 
 export default handleActions({
     [loadingJornadas]: (state,action) =>  {
-        console.log("Cambio  el estado loadingJornadas",action.payload)
         return action.payload
     }
 }, true)
