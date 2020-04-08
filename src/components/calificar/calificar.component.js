222333ed27664eec914217a83cf70f8a6cd3e109
Deiver <deiver.guerra.2017@upb.edu.co>
2020-04-07 22:45:49 -0500

    Errores de posicionamiento corregidos

--- src/components/calificar/calificar.component.js
+++ src/components/calificar/calificar.component.js
@@ -89,14 +89,14 @@ const CalfificarComponent = props => {
     }
 
     return (
-           <View style={{flex:1 , justifyContent: 'center', alignItems: 'center' }}>
+           <View style={{flex:1 , justifyContent: 'center' }}>
                 {props.granted && (
-                  <React.Fragment>
+                  <View  style={{flex:1}}>
                     { !props.ok && !props.hasQrCode && (
-                   <View style={{flex: 1,backgroundColor: '#000000'}}>
+                   <View style={{backgroundColor: '#000000', flex:1}}>
                      <BarCodeScanner
                       onBarCodeScanned={scanned ? undefined : props.handleBarCodeScanned}
-                      style={[StyleSheet.absoluteFillObject]}
+                      style={[StyleSheet.absoluteFillObject, {width:Dimensions.get('window').width, height:heightScreen, padding:0, margin:0}]}
                     />
                     <Animated.View 
                       style={{position:'relative', top:'50%', height:1, backgroundColor:'red',
@@ -115,121 +115,124 @@ const CalfificarComponent = props => {
                    </View>
                 )}
                 {props.ok && props.valid && (
-                  <View style={{backgroundColor:'#F1F2F6', width:'90%',marginTop:Platform.OS === 'adroid' ? '10%':heightStatusBar,
-                  borderRadius:20, alignItems:'center',
-                  shadowColor: "#000",
-                  shadowOffset: {
-                      width: 0,
-                      height: 2,
-                  },
-                  shadowOpacity: 0.25,
-                  shadowRadius: 3.84,
-                  elevation: 5,
-                    ...Platform.select({
-                    ios: {
-                        height:heightScreen  - 50
-                    },
-                    android: {
-                        height:heightScreen  - 50
-                    }})}}>
-                   
-                    <KeyboardAvoidingView 
-                     behavior="padding" >
-                      <Text style={{fontSize:20, color:'grey', textAlign:'center', marginVertical:10}}>{props.comment ? 'Comentarios':'Preguntas'}</Text>
-                      <Divider style={{marginBottom:5}}/>
-                      <ScrollView showsVerticalScrollIndicator={false}>
-                        {!props.comment && (
-                          <View style={{flex:1, borderRadius:20, flexDirection:'column', alignItems:'center',
-                            marginVertical:10}}>
-                            {props.presentacion.preguntas.length == 0 && (
-                              <Text style={{color:'#95A5A6', fontSize:20, textAlign:'center', marginVertical:20}}>Sé el primero en decir algo!.</Text>
-                            )}
-
-                            {props.presentacion.preguntas.map((pregunta, i) => (
-                              <View key={i.toString()} style={{backgroundColor:'white', width:Dimensions.get('window').width - 50,
-                              borderRadius:20, padding:10, marginVertical:5}}>
-                                      <Text style={{color:'#95A5A6'}}>{pregunta.contenido}</Text>
-                              </View>
-                            ))}
-
-                          </View>
-                        )}
+                  <View style={{ alignItems:'center'}}>
+                        <View style={{backgroundColor:'#F1F2F6', width:'90%',marginTop:Platform.OS === 'adroid' ? '10%':heightStatusBar,
+                        borderRadius:20, alignItems:'center',
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 2,
+                        },
+                        shadowOpacity: 0.25,
+                        shadowRadius: 3.84,
+                        elevation: 5,
+                          ...Platform.select({
+                          ios: {
+                              height:heightScreen  - 50
+                          },
+                          android: {
+                              height:heightScreen  - 50
+                          }})}}>
                         
-                        {props.comment && (
-                          <View style={{flex:1, borderRadius:20, flexDirection:'column', alignItems:'center',
-                            marginVertical:10}}>
-                            {props.presentacion.comentarios.length == 0 && (
-                              <Text style={{color:'#95A5A6', fontSize:20, textAlign:'center', marginVertical:20}}>Sé el primero en decir algo!.</Text>
-                            )}
-                            {props.presentacion.comentarios.map((comentario, i) => (
-                              <View key={i.toString()} style={{backgroundColor:'white', width:Dimensions.get('window').width - 50,
-                              borderRadius:20, padding:10, marginVertical:5}}>
-                                      <Text style={{color:'#95A5A6'}}>{comentario.contenido}</Text>
-                              </View>
-                            ))}
-                          </View>
-                        )}
-                      </ScrollView>
-                      
-                      <View style={styles.inputContainer}>
-                          <TextInput
-                          onChangeText={text => onChangeText(text)}
-                          autoCapitalize="sentences"
-                          autoCorrect={true}
-                          value={value}
-                          style={[styles.input, {marginBottom: focus ? 15: 10}]}
-                          multiline={true}
-                          autoFocus={true}
-                          placeholder="Escribe algo..."
-                          large={true}
-                          />
-                          <Button
-                          buttonStyle={{backgroundColor:'#0abde3', borderRadius:50, marginBottom: focus ? 15: 10, width:45,height:45,
-                          marginLeft:10}}
-                            icon={ <Icon
-                              name='paper-plane'
-                              size={18}
-                              color='white'
-                            />}
-                            onPress={() => {
-                              if(props.comment)
-                                  props.addComment(value)
-                              else
-                                  props.addQuestion(value)
-                              onChangeText('')
-                            }}
-                          />
-                      </View>
-                      
-                    </KeyboardAvoidingView >
-                  </View>
-                )}
-                { !props.valid &&  !props.scanned && (
-                  <View style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'center' }}>
-                      
-                        <Text style={{color:'red', fontSize:20, marginBottom:20}}>Código no valido</Text>
-                        <Image
-                          source={qrimage}
-                          style={{ width: 200, height: 200 }}
-                        />
-                        <Button 
-                          buttonStyle={{backgroundColor:'#0abde3', marginTop:20}}
-                          icon={
-                          <Icon
-                            name="qrcode"
-                            size={40}
-                            color="white"
-                          />
-                        }
-                        type="solid" 
-                        onPress={() => props.scanear()} ></Button>
-                      
-                  </View>
+                          <KeyboardAvoidingView 
+                          behavior="padding" >
+                            <Text style={{fontSize:20, color:'grey', textAlign:'center', marginVertical:10}}>{props.comment ? 'Comentarios':'Preguntas'}</Text>
+                            <Divider style={{marginBottom:5}}/>
+                            <ScrollView showsVerticalScrollIndicator={false}>
+                              {!props.comment && (
+                                <View style={{flex:1, borderRadius:20, flexDirection:'column', alignItems:'center',
+                                  marginVertical:10}}>
+                                  {props.presentacion.preguntas.length == 0 && (
+                                    <Text style={{color:'#95A5A6', fontSize:20, textAlign:'center', marginVertical:20}}>Sé el primero en decir algo!.</Text>
+                                  )}
+
+                                  {props.presentacion.preguntas.map((pregunta, i) => (
+                                    <View key={i.toString()} style={{backgroundColor:'white', width:Dimensions.get('window').width - 50,
+                                    borderRadius:20, padding:10, marginVertical:5}}>
+                                            <Text style={{color:'#95A5A6'}}>{pregunta.contenido}</Text>
+                                    </View>
+                                  ))}
+
+                                </View>
+                              )}
+                              
+                              {props.comment && (
+                                <View style={{flex:1, borderRadius:20, flexDirection:'column', alignItems:'center',
+                                  marginVertical:10}}>
+                                  {props.presentacion.comentarios.length == 0 && (
+                                    <Text style={{color:'#95A5A6', fontSize:20, textAlign:'center', marginVertical:20}}>Sé el primero en decir algo!.</Text>
+                                  )}
+                                  {props.presentacion.comentarios.map((comentario, i) => (
+                                    <View key={i.toString()} style={{backgroundColor:'white', width:Dimensions.get('window').width - 50,
+                                    borderRadius:20, padding:10, marginVertical:5}}>
+                                            <Text style={{color:'#95A5A6'}}>{comentario.contenido}</Text>
+                                    </View>
+                                  ))}
+                                </View>
+                              )}
+                            </ScrollView>
+                            
+                            <View style={styles.inputContainer}>
+                                <TextInput
+                                onChangeText={text => onChangeText(text)}
+                                autoCapitalize="sentences"
+                                autoCorrect={true}
+                                value={value}
+                                style={[styles.input, {marginBottom: focus ? 15: 10}]}
+                                multiline={true}
+                                autoFocus={true}
+                                placeholder="Escribe algo..."
+                                large={true}
+                                />
+                                <Button
+                                buttonStyle={{backgroundColor:'#0abde3', borderRadius:50, marginBottom: focus ? 15: 10, width:45,height:45,
+                                marginLeft:10}}
+                                  icon={ <Icon
+                                    name='paper-plane'
+                                    size={18}
+                                    color='white'
+                                  />}
+                                  onPress={() => {
+                                    if(props.comment)
+                                        props.addComment(value)
+                                    else
+                                        props.addQuestion(value)
+                                    onChangeText('')
+                                  }}
+                                />
+                            </View>
+                            
+                          </KeyboardAvoidingView >
+                        </View>
+                        </View>
+                        
+                      )}
+                      { !props.valid &&  !props.scanned && (
+                        <View style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'center' }}>
+                            
+                              <Text style={{color:'red', fontSize:20, marginBottom:20}}>Código no valido</Text>
+                              <Image
+                                source={qrimage}
+                                style={{ width: 200, height: 200 }}
+                              />
+                              <Button 
+                                buttonStyle={{backgroundColor:'#0abde3', marginTop:20}}
+                                icon={
+                                <Icon
+                                  name="qrcode"
+                                  size={40}
+                                  color="white"
+                                />
+                              }
+                              type="solid" 
+                              onPress={() => props.scanear()} ></Button>
+                            
+                        </View>
                 )}
                 {props.ok && !props.valid && (Alert.alert('Codigo invalido', 
                 'El codigo QR escaneado no es valido', 
                 [{ text: 'Cancelar'},{text: 'Scanear', onPress: () => props.scanear()}]))}
-                  </React.Fragment>
+                  </View>
                 )}
 
                 {!props.granted && (
