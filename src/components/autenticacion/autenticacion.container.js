895158705bef01dcac14ad9235f2bf3f6bf8e9fe
Alix Angarita <andreangarita1@hotmail.com>
2020-04-15 19:33:38 -0500

    correo tomado del input del login

--- src/components/autenticacion/autenticacion.container.js
+++ src/components/autenticacion/autenticacion.container.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import AuthenticationComponent from './autenticacion.component'
 
 export default class Authentication extends Component {
-
-    render(){
-        return <AuthenticationComponent/>
+    
+    render(){    
+        const { navigation } = this.props;
+        return <AuthenticationComponent
+        correo = {navigation.getParam('correo', '')}
+        />
     }
 }
