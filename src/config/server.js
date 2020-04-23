683ef4db39f67c62ee8a2b5c7395f95ecb40cc65
Deiver <deiver.guerra.2017@upb.edu.co>
2020-04-22 21:00:30 -0500

    server heroku

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
 export default {
-  host:"https://exporcheck-socket-server.herokuapp.com/",
+  host:"https://exporcheck-socket-server.herokuapp.com",
   rest:"https://expocheck-rest.herokuapp.com"
 }
